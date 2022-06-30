e42e569010514cd595e8e712258cb336c1f68280
RP-01? <xxsomethingcleverxx3@gmail.com>
2022-06-29 21:29:36 -0500

    Update OsintSeeker.sh

--- OsintSeeker.sh
+++ OsintSeeker.sh
@@ -4,8 +4,6 @@ url=$1
 
 echo "This is your OSINT tool for gathering domain information and emails! Happy Hunting"
 
-#You need to customize the output locations of your directory below
-
 if [ ! -d "/home/kali/Desktop/Engagement" ];then
 	mkdir "/home/kali/Desktop/Engagement"
 fi
@@ -13,6 +11,8 @@ fi
 echo "Directory Permissions set..."
 chmod 777 /home/kali/Desktop/Engagement/
 
+echo "[+] NSLOOKUP is running....."
+nslookup $url >> home/kali/Desktop/Engagement/NSLOOKUP.txt
 
 echo "[+] Assetfinder is running....."
 assetfinder $url >> /home/kali/Desktop/Engagement/assetFinderOutput.txt
@@ -29,3 +29,11 @@ python3 crosslinked.py -f '{first}.{last}@domain.com' $url >> /home/kali/Desktop
 
 echo "[+]Nikto is running....."
 nikto -h $url >> /home/kali/Desktop/Engagement/niktoOutput.txt
+
+echo " The enumeration is done! Now the port scanning begins"
+# NMAP will now grep out IPs from amass and export them to a file, that file will then be uploaded for a port scan in NMAP"
+
+grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" amass_company_1.txt | sort -u >>ips.txt
+
+echo "NMAP is scanning  0...0   "
+nmap -T4 -A -sV -iL /home/kali/Desktop/Engagement/ips.txt >> /home/kali/Desktop/Engagement/NMAP Results
