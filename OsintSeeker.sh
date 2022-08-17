5031a2f4a6c3a15cb024c1cc585a1356003f352a
RP-01? <xxsomethingcleverxx3@gmail.com>
2022-08-17 16:14:02 -0500

    Update OsintSeeker.sh

--- OsintSeeker.sh
+++ OsintSeeker.sh
@@ -18,36 +18,44 @@ echo ""
 echo ""
 echo "This is your OSINT tool for gathering domain information and emails! Happy Hunting"
 
-if [ ! -d "/home/kali/Desktop/Engagement" ];then
-	mkdir "/home/kali/Desktop/Engagement"
+if [ ! -d "/home/kali/Desktop/Engagement-$url" ];then
+	mkdir "/home/kali/Desktop/Engagement-$url" 
 fi
 
-echo "Directory Permissions set..."
-chmod 777 /home/kali/Desktop/Engagement/
+echo "Directory Permissions setting..."
+chmod 777 /home/kali/Desktop/Engagement-$url
+echo "Directory Permissions Done..."
 
 echo "[+] NSLOOKUP is running....."
-nslookup $url >> home/kali/Desktop/Engagement/NSLOOKUP.txt
+nslookup $url >> /home/kali/Desktop/Engagement-$url/NSLOOKUP.txt
+echo "NSLOOKUP is Done..."
 
 echo "[+] Assetfinder is running....."
-assetfinder $url >> /home/kali/Desktop/Engagement/assetFinderOutput.txt
-
+assetfinder $url >> /home/kali/Desktop/Engagement-$url/assetFinderOutput.txt
+echo "Assetfinder is Done..."
 
 echo "[+] Amass is running, take a breather ;)....."
-amass enum -active -d $url -src -ip -dir /home/kali/Desktop/Engagement/ -o /home/kali/Desktop/Engagement/amass_company_1.txt
+amass enum -active -d $url -src -ip -dir /home/kali/Desktop/Engagement-$url -o /home/kali/Desktop/Engagement-$url/amass_company_1.txt
+echo "Amass is Done..."
 
 echo "[+] Sublist3r is running....."
-sublist3r -d $url >> /home/kali/Desktop/Engagement/sublist3rOutput.txt
+sublist3r -d $url >> /home/kali/Desktop/Engagement-$url/sublist3rOutput.txt
+echo "Sublist3r is Done..."
 
 echo "[+] Email gathering from LinkedIn is running....."
-python3 crosslinked.py -f '{first}.{last}@domain.com' $url >> /home/kali/Desktop/Engagement/LinkedInEmails.txt
+python3 crosslinked.py -f '{first}.{last}@domain.com' $url >> /home/kali/Desktop/Engagement-$url/LinkedInEmails.txt
+echo "Crosslinked is Done..."
 
 echo "[+]Nikto is running....."
-nikto -h $url >> /home/kali/Desktop/Engagement/niktoOutput.txt
+nikto -h $url >> /home/kali/Desktop/Engagement-$url/niktoOutput.txt
+echo "Nikto is Done..."
 
 echo " The enumeration is done! Now the port scanning begins"
 # NMAP will now grep out IPs from amass and export them to a file, that file will then be uploaded for a port scan in NMAP"
+echo "Port Scanning is Done..."
 
 grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" amass_company_1.txt | sort -u >>ips.txt
 
 echo "NMAP is scanning  0...0   "
-nmap -T4 -A -sV -iL /home/kali/Desktop/Engagement/ips.txt >> /home/kali/Desktop/Engagement/NMAP Results
+nmap -T4 -A -sV -iL /home/kali/Desktop/Engagement-$url/ips.txt >> /home/kali/Desktop/Engagement-$url/NMAP Results
+echo "NMAP is Done..."
