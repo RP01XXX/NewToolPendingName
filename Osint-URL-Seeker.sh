fb6f6ae4a6c88ae9aebb015cf5aff785eddbb64c
RP-01? <xxsomethingcleverxx3@gmail.com>
2022-10-18 09:57:22 -0500

    Update Osint-URL-Seeker.sh

--- Osint-URL-Seeker.sh
+++ Osint-URL-Seeker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-url=$1
+echo "Enter Your url"
+read url
+
+echo "Company Name"
+read Name
+
 echo ""
 echo ""
 echo ""
@@ -17,58 +22,56 @@ echo "This is your OSINT tool for gathering domain information and emails! Happy
 # Need to add dirb or gobuster (metasploit maybe?, spiderfoot, harvester? Website enumeration like data from pages?
 
 #Change this pathway to your personal location.
-if [ ! -d "/home/kali/Desktop/Engagement-$url" ];then
-	mkdir "/home/kali/Desktop/Engagement-$url" 
+if [ ! -d "/home/kali/Desktop/Engagement-$Name" ];then
+	mkdir "/home/kali/Desktop/Engagement-$Name" 
 fi
 
-echo "Directory Permissions setting..."
-chmod 777 /home/kali/Desktop/Engagement-$url
-cd /home/kali/Desktop/Engagement-$url
-echo "Directory Permissions Done..."
+
 
 echo "[+] NSLOOKUP is running....."
-nslookup $url >> /home/kali/Desktop/Engagement-$url/NSLOOKUP.txt
+nslookup $url >> /home/kali/Desktop/Engagement-$Name/NSLOOKUP.txt
 echo "NSLOOKUP is Done..."
 
 echo "[+] Assetfinder is running....."
-assetfinder $url >> /home/kali/Desktop/Engagement-$url/assetFinderOutput.txt
+assetfinder $url >> /home/kali/Desktop/Engagement-$Name/assetFinderOutput.txt
 echo "Assetfinder is Done..."
 
 #Amass Finds all Subdomains and IP addresses, not unique and is grepped out later
 echo "[+] Amass is running, take a breather ;)....."
-amass enum -active -d $url -src -ip -dir /home/kali/Desktop/Engagement-$url -o /home/kali/Desktop/Engagement-$url/AmassSubDomains.txt
-rm /home/kali/Desktop/Engagement-$url/amass.log
-rm /home/kali/Desktop/Engagement-$url/amass.json
-rm /home/kali/Desktop/Engagement-$url/indexes.bolt
+amass enum -active -d $url -src -ip -dir /home/kali/Desktop/Engagement-$Name -o /home/kali/Desktop/Engagement-$Name/AmassSubDomains.txt
+rm /home/kali/Desktop/Engagement-$Name/amass.log
+rm /home/kali/Desktop/Engagement-$Name/amass.json
+rm /home/kali/Desktop/Engagement-$Name/indexes.bolt
 echo "Amass is Done..."
 
 #Second look at subdomains with this tool
 echo "[+] Sublist3r is running....."
-sublist3r -d $url >> /home/kali/Desktop/Engagement-$url/sublist3rOutput.txt
+sublist3r -d $url >> /home/kali/Desktop/Engagement-$Name/sublist3rOutput.txt
 echo "Sublist3r is Done..."
 
 #Nikto Vulnerability Scan
 echo "[+]Nikto is running....."
-nikto -h $url >> /home/kali/Desktop/Engagement-$url/niktoOutput.txt
+nikto -h $url >> /home/kali/Desktop/Engagement-$Name/niktoOutput.txt
 echo "Nikto is Done..."
 
 # NMAP will now grep out IPs from amass and export them to a file, that file will then be uploaded for a port scan in NMAP"
 echo " The enumeration is done! Now the port scanning begins"
 grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" AmassSubDomains.txt | sort -u >>ips.txt
 echo "NMAP is scanning  0...0   "
-nmap -T4 -A -sV -iL /home/kali/Desktop/Engagement-$url/ips.txt >> /home/kali/Desktop/Engagement-$url/NMAP Results
+nmap -T4 -A -sV -iL /home/kali/Desktop/Engagement-$Name/ips.txt >> /home/kali/Desktop/Engagement-$Name/NMAP Results
 echo "Port Scanning is Done..."
 
 #CRT.SH searching for interesting subdomain Locations, I would like to take the outputs of Amass and play here more as an input.
-curl -s https://crt.sh/?q=$url > /home/kali/Desktop/Engagement-$url/crtfirst.txt
-cat /home/kali/Desktop/Engagement-$url/crtfirst.txt | grep $TARGET | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sort -u > /home/kali/Desktop/Engagement-$url/$TARGET-crt.txt
-curl -s https://crt.sh/?q=*.$url > /home/kali/Desktop/Engagement-$url/crtsecond.txt
-cat /home/kali/Desktop/Engagement-$url/crtsecond.txt | grep $TARGET | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sort -u > /home/kali/Desktop/Engagement-$url/$TARGET-crt2.txt
+curl -s https://crt.sh/?q=$url > /home/kali/Desktop/Engagement-$Name/crtfirst.txt
+cat /home/kali/Desktop/Engagement-$Name/crtfirst.txt | grep $TARGET | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sort -u > /home/kali/Desktop/Engagement-$Name/$TARGET-crt.txt
+curl -s https://crt.sh/?q=*.$url > /home/kali/Desktop/Engagement-$Name/crtsecond.txt
+cat /home/kali/Desktop/Engagement-$Name/crtsecond.txt | grep $TARGET | grep TD | sed -e 's/<//g' | sed -e 's/>//g' | sed -e 's/TD//g' | sed -e 's/\///g' | sed -e 's/ //g' | sed -n '1!p' | sort -u > /home/kali/Desktop/Engagement-$Name/$TARGET-crt2.txt
 
 #Can I copy all file data and place into 1 file and then delete the file left over to have 1 file?
-rm /home/kali/Desktop/Engagement-$url/crtfirst.txt 
-rm /home/kali/Desktop/Engagement-$url/crtsecond.txt 
+rm /home/kali/Desktop/Engagement-$Name/crtfirst.txt 
+rm /home/kali/Desktop/Engagement-$Name/crtsecond.txt 
 
-
-#THIS ISNT WORKING EVEN AS ROOT
-sudo chmod 777 home/kali/Desktop/Engagement-$url
+echo "Directory Permissions setting..."
+chmod 777 /home/kali/Desktop/Engagement-$Name
+cd /home/kali/Desktop/Engagement-$Name
+echo "Directory Permissions Done..."
