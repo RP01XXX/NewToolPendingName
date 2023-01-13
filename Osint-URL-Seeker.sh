379e59fa205e77da0bdc49a2245808de1ccd21bf
RP-01? <xxsomethingcleverxx3@gmail.com>
2023-01-13 09:01:12 -0600

    Update Osint-URL-Seeker.sh

--- Osint-URL-Seeker.sh
+++ Osint-URL-Seeker.sh
@@ -29,23 +29,23 @@ if [ ! -d "$path/$Name" ];then
 	mkdir "$path/$Name/" 
 fi
 
-echo "[1/] NSLOOKUP is running....."
+echo "[1] NSLOOKUP is running....."
 nslookup $url >> $path/$Name/NSLOOKUP.txt
 echo "NSLOOKUP is Done..."
 
-echo "[1/] Assetfinder is running....."
+echo "[2] Assetfinder is running....."
 assetfinder $url >> $path/$Name/assetFinderOutput.txt
 echo "Assetfinder is Done..."
 
-echo "[1/]  Whatweb is running......."
+echo "[3]  Whatweb is running......."
 whatweb $url --aggression 3 -v --no-errors --log-verbose=$path/$Name/WWebResults.txt
 
 #SSL Scan 
-echo "Scanning for SSL Vulnerabilities....."
+echo "[4] Scanning for SSL Vulnerabilities....."
 sslyze $url >> $path/$Name/sslfindings.txt
 
 #Amass Finds all Subdomains and IP addresses, not unique and is grepped out later
-echo "[4/] Amass is running, take a breather ;)....."
+echo "[5] Amass is running, take a breather ;)....."
 amass enum -active -d $url -src -ip -dir $path/$Name/ -o $path/$Name/AmassSubDomains.txt
 rm $path/$Name/amass.log
 rm $path/$Name/amass.json
@@ -54,25 +54,45 @@ amass viz -d3  -d $url
 echo "Amass is Done..."
 
 # DNS Recon Run
-echo "[5/] DNS Recon is running.........."
+echo "[6] DNS Recon is running.........."
 dnsrecon -d $url -a -c $path/$Name/
 
 # DIG
+echo "[7] DIG is digging......."
 dig $url > $path/$Name/digResults.txt
 
 # The Harvester
-echo "[6/] TheHarvester is running............."
+echo "[8] TheHarvester is running............."
 theHarvester -d <URL> -l 200 -b bing,bingapi,certspotter,dnsdumpster,sublist3r,urlscan,virustotal -f $path/$Name/Harvested.txt
+
+echo "Enter Your Wordlist for Subdomain Brute Forcing"
+read wordlist
+
+echo "[9] Brute Forcing Subdomains........"
+knock.py $url -w $wordlist -o $path/$url
+
+echo "[10] Running Nikto..............."
+nikto -host $url
+
+echo "[11] Curl options ......."
+curl -X options $url
+
+echo "[12] Is it Wordpress?..........."
+wpscan --url $url --wp-content-dir /wp-content/ --enumerate vp --plugins-detection aggressive --enumerate u -o "$path/$Name/WordpressScan.txt"
+
+echo "[Final] Is there a WAF?......"
+wafw00f $url ?? $path/$Name/WAF.txt
 
 echo "Cleaning up your results"
 
-echo "identify unique IP's from  Amass"
+echo "[8/] identify unique IP's from  Amass"
 grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" $path/$Name/AmassSubDomains.txt | sort -u >> $path/$Name/UniqueAmassIps.txt
 rm $path/$Name/AmassSubDomains.txt
 
-echo "identify unique IP's from  NSLOOKUP"
+echo "[9/] identify unique IP's from  NSLOOKUP"
 grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" $path/$Name/NSLOOKUP.txt | sort -u >> $path/$Name/NSIPs.txt
 rm $path/$Name/NSLOOKUP.txt
+
 
 
 #This stops here as you need to verify if all IPs found are in scope or not.
